ad182ea58218a4b77856cde09814ed2a841297d9
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-06-25 22:40:51 -0700

    Update p_underwaterSub.cs

--- Assets/Scripts/p_underwaterSub.cs
+++ Assets/Scripts/p_underwaterSub.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.Experimental.GlobalIllumination;
 
 public class p_underwaterSub : MonoBehaviour
 {
     Animator _anim;
     [SerializeField] GameObject _spot;
-
-    Rigidbody _rb;
+    [SerializeField] Transform _grabber;
     Vector2 _inputs;
     Vector2 _lookCoOrds;
     Vector2 _lookStorage;
-
+    Vector3 _objPos;
     [SerializeField] GameObject _body;
-    float _groundCheckDistance = 0.6f;
-    [SerializeField] float _raiseUp;
+    [SerializeField] float _ballast;
+    [SerializeField] float _carryDistance;
+    [SerializeField] float _carrySpeed = 1f;
     private readonly float _lookSensitivity = 2;
     private readonly float _speedWalk = 3;
     private readonly float _speedRun = 5;
@@ -24,11 +25,9 @@ public class p_underwaterSub : MonoBehaviour
     bool _isInvert = false;
 
     GameObject _obj;
-    RaycastHit _hit;
     private void Awake()
     {
         if (!_anim) _anim = GetComponent<Animator>();
-        //if (!_rb) _rb = GetComponentInParent<Rigidbody>();
     }
     // Start is called before the first frame update
     void Start()
@@ -41,25 +40,25 @@ public class p_underwaterSub : MonoBehaviour
     {
         _inputs = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         _lookCoOrds = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        _objPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _grabber.localPosition = new Vector3(_objPos.x, _objPos.y, _carryDistance);
+        
         if (_isRun) _speedCur = _speedRun;
         else _speedCur = _speedWalk;
 
-
         if (Input.GetButton("Run")) _isRun = true;
-        else
-            _isRun = false;
+        else _isRun = false;
 
         if (Input.GetMouseButton(1)) Cursor.lockState = CursorLockMode.None;
-        else
-            Cursor.lockState = CursorLockMode.Locked;
+        else Cursor.lockState = CursorLockMode.Locked;
+       
+
 
         if (Input.GetMouseButtonDown(0))
             MouseFunctions();
-        else if (Input.GetMouseButton(0))
-            _obj.transform.parent = transform;
-        if (Input.GetMouseButtonUp(0) && _obj )
-            _obj.transform.parent = null;
-
+        else if (Input.GetMouseButton(0))        
+            _obj.transform.localPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + _carryDistance);
+        
         _anim.SetBool("isRun", _isRun);
         _anim.SetFloat("speed", Mathf.Abs(_inputs.y));
     }
@@ -67,20 +66,24 @@ public class p_underwaterSub : MonoBehaviour
     private void FixedUpdate()
     {
         Walking(_speedCur);
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) _carryDistance += _carrySpeed * Time.deltaTime;
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0) _carryDistance -= _carrySpeed * Time.deltaTime;
     }
 
     private void LateUpdate()
     {
+
         if (!_isInvert)
         {
             _spot.transform.rotation = Quaternion.Euler(_lookStorage.y * -1, _lookStorage.x, 0.0f);
-            _body.transform.rotation = Quaternion.Slerp(_body.transform.rotation, Quaternion.Euler(_lookStorage.y * -1, _lookStorage.x, 0.0f), 1 * Time.deltaTime);
+            if (Cursor.lockState == CursorLockMode.Locked)
+                _body.transform.rotation = Quaternion.Slerp(_body.transform.rotation, Quaternion.Euler(0.0f, _lookStorage.x, 0.0f), 1 * Time.deltaTime);
         }
         else
         {
             _spot.transform.rotation = Quaternion.Euler(_lookStorage.y * 1, _lookStorage.x, 0.0f);
-            _body.transform.rotation = Quaternion.Slerp(_body.transform.rotation, Quaternion.Euler(_lookStorage.y * 1, _lookStorage.x, 0.0f), 1 * Time.deltaTime); 
-
+            if (Cursor.lockState == CursorLockMode.Locked)
+                _body.transform.rotation = Quaternion.Slerp(_body.transform.rotation, Quaternion.Euler(0.0f, _lookStorage.x, 0.0f), 1 * Time.deltaTime);
         }
     }
 
@@ -88,7 +91,6 @@ public class p_underwaterSub : MonoBehaviour
     {
         _lookStorage += _lookCoOrds * _lookSensitivity;
         _body.transform.Translate(((Vector3.right * _inputs.x) + (Vector3.forward * _inputs.y)) * speed * Time.deltaTime);
-      //  _body.transform.rotation = Quaternion.Euler(_lookStorage.y, _lookStorage.x, 0.0f);
     }
 
     RaycastHit MouseFunctions()
@@ -97,9 +99,12 @@ public class p_underwaterSub : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
-            _obj = hit.collider.gameObject;
+            if (Vector3.Distance(hit.collider.transform.position,transform.position) < _carryDistance + 0.5f || Vector3.Distance(hit.collider.transform.position, transform.position) > _carryDistance - 0.5f)
+            {
+                _obj = hit.collider.gameObject;
+                _objPos = hit.transform.position;
+            }
         }
-
         return hit;
     }
 }
