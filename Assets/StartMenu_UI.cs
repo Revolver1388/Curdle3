26b2ce197112c89860ec70ba0ecd7be429e9e2a9
Matt Riddett <35952057+lazyninjacat@users.noreply.github.com>
2020-06-26 14:15:11 -0700

    basic buttons and functionality into start menu scene and about scene.

---
+++ Assets/StartMenu_UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu_UI : MonoBehaviour
+{
+   public void StartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void AboutButton()
+    {
+        SceneManager.LoadScene("AboutScene");
+    }
+}
