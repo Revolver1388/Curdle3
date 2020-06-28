ef4822fa6d0933bb17d16c584ea398f74f2d4934
BurntPudding <66089829+BurntPudding@users.noreply.github.com>
2020-06-28 12:03:46 -0600

    Made pause menu and title screen

---
+++ Assets/PauseMenu/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities.UniversalDelegates;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) 
+        {
+            Pause();
+        }
+    }
+    public void Pause()
+    {
+        if (PauseUI.activeSelf)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+
+    }
+    public void Resume() 
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit.");
+    }
+}
